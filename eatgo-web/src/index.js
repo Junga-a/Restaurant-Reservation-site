f9da5a836f39347b0fe5dd2d208f0c6c45e87044
Junga-a <goqkrtl97@naver.com>
2021-01-02 02:24:22 +0900

    seventh commit

--- eatgo-web/src/index.js
+++ eatgo-web/src/index.js
@@ -1,16 +1,17 @@
-(async()=>{
-    const url= 'http://localhost:8080/restaurants';
-    const response=await fetch(url);
+(async () => {
+    const url = 'http://localhost:8080/restaurants';
+    const response = await fetch(url);
     const restaurants = await response.json();
 
-    const element=document.getElementById('app');
-    element.innerHTML='
+    const element = document.getElementById('app');
+    element.innerHTML = '
         ${restaurants.mpp(restaurant => '
             <p>
                 ${restaurant.id}
-                ${restaurant.name}
-                ${restaurant.address}
-            </p>
-        ')jion('')}
-        ';
+        ${restaurant.name}
+        ${restaurant.address}
+        < /p>
+    ')jion('
+    ')}
+    ';
 })();
