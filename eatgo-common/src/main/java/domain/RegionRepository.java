88c79088c9b0614fb900000022727130123bae26
Junga-a <goqkrtl97@naver.com>
2021-01-05 23:00:33 +0900

    12th commit

--- eatgo-common/src/main/java/domain/RegionRepository.java
+++ eatgo-common/src/main/java/domain/RegionRepository.java
@@ -1,4 +1,13 @@
 package domain;
 
-public interface RegionRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface RegionRepository extends CrudRepository<Region, Long> {
+
+    List<Region> findAll();
+
+    Region save(Region region);
+
 }
